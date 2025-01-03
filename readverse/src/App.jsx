a9f99e596d62f4baf494383da0d9bb75e37fdb49
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2025-01-03 17:42:37 -0300

    Reiniciando o projeto

--- readverse/src/App.jsx
+++ readverse/src/App.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import Header from './components/header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //Rotas
-import Home from './pages/home/Home';
-import Books from './pages/books/Books';
-import Authors from './pages/authors/Authors';
-import Articles from './pages/articles/Articles';
-import About from './pages/about/About';
-
+import Home from './pages/Home';
 function App() {
 
   return (
     <BrowserRouter>
-      <Header />
-
       <Routes>
         <Route path='/' exact element={<Home />}></Route>
-        <Route path='/books' exact element={<Books/>}></Route>
-        <Route path='/authors' exact element={<Authors />}></Route>
-        <Route path='/articles' exact element={<Articles />}></Route>
-        <Route path='/about' exact element={<About />}></Route>
       </Routes>
-      
-      
     </BrowserRouter>
  
   )
