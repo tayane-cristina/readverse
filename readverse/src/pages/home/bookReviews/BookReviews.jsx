c403cbfdd5dcce5e07ad62017146493df7dcec20
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2025-01-24 10:29:55 -0300

    criando um modal para a página de resenhas

--- readverse/src/pages/home/bookReviews/BookReviews.jsx
+++ readverse/src/pages/home/bookReviews/BookReviews.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import { ReviewsData } from '../../../data/reviews/ReviewsData';
-import TextLimited from '../../../components/TextLimited'
+import TextLimited from '../../../components/TextLimited';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 const BookReviews = () => {
 
     const [startIndex, setStartIndex] = useState(0);
     const [finalIndex, setFinalIndex] = useState(4);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedReview, setSelectedReview] = useState(null);
+
+    const openModal = (review) => {
+        setIsModalOpen(true);
+        setSelectedReview(review)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setSelectedReview(null)
+    } 
 
     const next = () => {
         if (startIndex > ReviewsData.length - 5) {
@@ -44,11 +59,40 @@ return(
                     <p><strong>{review.title}</strong></p>
                     <TextLimited text={review.reviewText} limit={200}/>
                     <p><strong>Por quê ler este livro:</strong> {review.incentive}</p>
+                    <button onClick={() => openModal(review)}>Ler resenha</button>
                 </li>
                 
             ))}
         <button onClick={next}>Próximo</button>
         </ul>
+
+        {selectedReview && (
+            <Modal
+            isOpen={isModalOpen}
+            onRequestClose={closeModal}
+            contentLabel="Resenha Completa"
+            style={{
+                content: {
+                    top: '50%',
+                    left: '50%',
+                    right: 'auto',
+                    bottom: 'auto',
+                    marginRight: '-50%',
+                    transform: 'translate(-50%, -50%)',
+                },
+                overlay: {
+                    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+                },
+            }}
+            >
+                <h2>{selectedReview.title}</h2>
+                <img src={selectedReview.cover} alt={`${selectedReview.title}-image`} className='review-book-cover'></img>
+                <p><strong>Resenha Completa:</strong></p>
+                <p>{selectedReview.reviewText}</p>
+                <p><strong>Por quê ler este livro:</strong> {selectedReview.incentive}</p>
+                <button onClick={closeModal}>Fechar</button>
+            </Modal>
+        )}
     </div>
 
 )
